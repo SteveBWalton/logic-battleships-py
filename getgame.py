e95c3cccd87d4899c5ebce704e78d72aaa8fad53
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2019-12-20 08:12:29 +0000

    Added game 46.

--- getgame.py
+++ getgame.py
@@ -685,15 +685,46 @@ def getGame(index, args):
         game.vertical[7] = 2
         game.vertical[8] = 5
 
-        # Try the super tanker in row 4, attempt 1. No Match.
-        # Try the super tanker in row 4, attempt 2. Not possible.
-        # Try the super tanker in row 4, attempt 3. Not possible.
-        # Try the super tanker in row 4, attempt 4. No Match.
+        game.mask[0] = 0
+        game.mask[1] = 0
+        game.mask[2] = 0
+        game.mask[3] = 0
+        game.mask[4] = 0
+        game.mask[5] = 0
+        game.mask[6] = 0
+        game.mask[7] = 0
+        game.mask[8] = 0
 
-        # Try the super tanker in column 2, attempt 1 (top). No Match.
-        # Try the super tanker in column 2, attempt 2 (bottom). No Match.
+        game.negativeMask[0] = 0
+        game.negativeMask[1] = 0
+        game.negativeMask[2] = 0
+        game.negativeMask[3] = 0
+        game.negativeMask[4] = 0
+        game.negativeMask[5] = 0
+        game.negativeMask[6] = 0
+        game.negativeMask[7] = 0
+        game.negativeMask[8] = 0
 
-        # Try the super tanker in column 9, attempt 1 (top).
+    elif index == 46:
+        game = Battleships(index, 9, 5, 'Logic Problems Battleships Number 46', args)
+        game.horizontal[0] = 2
+        game.horizontal[1] = 3
+        game.horizontal[2] = 3
+        game.horizontal[3] = 4
+        game.horizontal[4] = 2
+        game.horizontal[5] = 3
+        game.horizontal[6] = 3
+        game.horizontal[7] = 4
+        game.horizontal[8] = 1
+        game.vertical[0] = 3
+        game.vertical[1] = 2
+        game.vertical[2] = 3
+        game.vertical[3] = 5
+        game.vertical[4] = 1
+        game.vertical[5] = 1
+        game.vertical[6] = 7
+        game.vertical[7] = 1
+        game.vertical[8] = 2
 
         game.mask[0] = 0
         game.mask[1] = 0
